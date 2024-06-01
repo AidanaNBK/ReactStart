17a58fb7119d969a670874083effda20521a95ac
Aidana K <aidana.amnbk@gmail.com>
2024-06-01 16:06:20 +0500

    feat: add genRandom to generate random ints

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
+const reactDescr = ["Fundamentals", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.randon() * (max + 1));
+}
+
+function Header() {
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+// <Header /> is a way to call the function in regular jsx component
+// or <Header> </Header>
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>

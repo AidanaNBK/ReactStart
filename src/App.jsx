fa69260e2ba9667a91ddb38bab9e7b1c14402261
Aidana K <aidana.amnbk@gmail.com>
2024-06-01 21:34:05 +0500

    feat: dynamic change of components

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,13 @@ import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 import { CORE_CONCEPTS } from "./data.js";
+import { EXAMPLES } from "./data.js";
+import "./index.css";
+
 // <Header /> is a way to call the function in regular jsx component
 // or <Header> </Header>
 function App() {
-  let [selectedTopic, setSelectedTopic] = useState("Please click a button");
+  let [selectedTopic, setSelectedTopic] = useState("components");
 
   // let tabContent = "Please click a button";
 
@@ -33,7 +36,7 @@ function App() {
         </section>
         {/* <h2>Time to get started!</h2> */}
       </main>
-      <section id="examples">
+      <section id="example">
         <h2>Examples</h2>
         <menu>
           <TabButton onSelect={() => handleClick("components")}>
@@ -43,7 +46,14 @@ function App() {
           <TabButton onSelect={() => handleClick("props")}>Props</TabButton>
           <TabButton onSelect={() => handleClick("state")}>State</TabButton>
         </menu>
-        {selectedTopic}
+
+        <div id="tab-content">
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>{EXAMPLES[selectedTopic].code}</code>
+          </pre>
+        </div>
       </section>
     </div>
   );
